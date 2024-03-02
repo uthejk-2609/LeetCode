c70007d52e765380ce323e1fcd518c81eef543f3
UTHEJ K <121709203+uthejk-2609@users.noreply.github.com>
2024-03-02 10:17:32 +0530

    66. Plus One.py

---
+++ 66. Plus One.py
@@ -0,0 +1,17 @@
+def plusOne(self, digits):
+        digits = digits[::-1]
+        one, i = 1, 0
+
+        while one:
+            if i < len(digits):
+                if digits[i] == 9:
+                    digits[i] = 0
+                else:
+                    digits[i] += 1
+                    one = 0
+            else:
+                digits.append(1)
+                one = 0
+            i += 1
+            
+        return digits[::-1]
