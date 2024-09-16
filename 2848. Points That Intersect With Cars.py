bb60cfc591d32224aa1a38502a047d2df060bf14
Uthej Karamalapudi <121709203+uthejk-2609@users.noreply.github.com>
2024-09-16 10:46:18 +0530

    Create 2848. Points That Intersect With Cars.py

---
+++ 2848. Points That Intersect With Cars.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numberOfPoints(self, nums: List[List[int]]) -> int:
+        nums.sort()
+        n = len(nums)
+        res = []
+        sum = 0
+
+        for i in range(n):
+            if len(res) == 0 or nums[i][0] > res[-1][1]:
+                res.append(nums[i])
+            else:
+                res[-1][1] = max(res[-1][1], nums[i][1])
+
+        for i in range(len(res)):
+            sum += (res[i][1] - res[i][0] + 1)
+
+        return sum
