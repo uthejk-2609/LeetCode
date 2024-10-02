c3909969a034f6cf5868cbf3c07cee4b09ceb658
Uthej Karamalapudi <121709203+uthejk-2609@users.noreply.github.com>
2024-10-02 15:26:01 +0530

    Create 295. Find Median from Data Stream.py

---
+++ 295. Find Median from Data Stream.py
@@ -0,0 +1,31 @@
+import heapq
+
+class MedianFinder:
+
+    def __init__(self):
+        self.left = []
+        self.right = []
+
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.left, -num)
+        
+        if self.left and self.right and (-self.left[0] > self.right[0]):
+            heapq.heappush(self.right, -heapq.heappop(self.left))
+
+
+        if len(self.left) > len(self.right) + 1:
+            heapq.heappush(self.right, -heapq.heappop(self.left))
+        elif len(self.right) > len(self.left):
+            heapq.heappush(self.left, -heapq.heappop(self.right))
+
+    def findMedian(self) -> float:
+        if len(self.left) > len(self.right):
+            return -self.left[0]
+        return (-self.left[0] + self.right[0]) / 2.0
+        
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
